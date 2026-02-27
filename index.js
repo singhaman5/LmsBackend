a9d12cbab7ea05b85bf392ecaee6b56afa7f6f2f
Aman Singh <amansinghx04@gmail.com>
2026-02-27 11:53:44 +0530

    Updated

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
+// import express from "express";
+// import cors from "cors";
+// import dbConnect from "./config/db.js";
+// import userRouter from "./routers/userRoute.js";
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use("/api/users", userRouter);
+
+// const startServer = async () => {
+//   await dbConnect();
+//   app.listen(8080, () => console.log("Server started on port 8080"));
+// };
+
+// startServer();
+// import express from "express";
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "LMS Backend running" });
+// });
+
+// export default app; // ✅ REQUIRED
 import express from "express";
-import cors from "cors";
 import dbConnect from "./config/db.js";
 import userRouter from "./routers/userRoute.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+let isConnected = false;
+
+app.use(async (req, res, next) => {
+  if (!isConnected) {
+    await dbConnect();
+    isConnected = true;
+  }
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "LMS Backend running" });
+});
 
 app.use("/api/users", userRouter);
 
-const startServer = async () => {
-  await dbConnect();
-  app.listen(8080, () => console.log("Server started on port 8080"));
-};
-
-startServer();
+export default app;
